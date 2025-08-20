f4bb4fadc597104827270b16977e446e2bc0e981
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-08-20 18:42:54 -0400

    test

--- cypress/e2e/add-todos.cy.js
+++ cypress/e2e/add-todos.cy.js
@@ -4,5 +4,9 @@ describe('Todo app', () => {
   it('hides the no-todos message when there are todos', () => {
     cy.request('POST', '/reset', { todos: [] })
     cy.visit('/')
+    cy.get('.no-todos').should('be.visible')
+    cy.get('.new-todo').type('write code{enter}')
+    cy.get('.new-todo').type('write tests{enter}')
+    cy.get('.no-todos').should('not.be.visible')
   })
 })
